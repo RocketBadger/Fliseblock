41f3ceb777053b9e42435e2e8d1eed7d931f493a
Tais Merrild <merrildt@gmail.com>
2021-11-02 21:30:40 +0800

    more tests

--- Test.py
+++ Test.py
@@ -4,10 +4,14 @@ robbie = Raspblock()
 
 count = 0
 
-while True: 
-    if count > 3000:
-        count = 0
-    Raspblock.Servo_control(robbie, count, 0)
+# while True: 
+#     if count > 3000:
+#         count = 0
+#     Raspblock.Servo_control(robbie, count, 0)
+#     count = count + 1
+#     print(count)
+while count < 100:
+    # Raspblock.Speed_axis_control(robbie, 0, 10, 0)
+    # Raspblock.Speed_axis_Yawhold_control(robbie, -10, -10)
     count = count + 1
     print(count)
-    
