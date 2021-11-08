579fae07ebf775f1cdbf1c941293869eb4f47f5f
Tais <55287175+RocketBadger@users.noreply.github.com>
2021-11-08 15:02:08 +0100

    Added cam thread, and reorganized

--- Fliseblock/send_cam.py
+++ Fliseblock/send_cam.py
@@ -1,26 +1,28 @@
 import base64
 import cv2
 import zmq
+import threading
 
-context = zmq.Context()
-footage_socket = context.socket(zmq.PUB)
-footage_socket.connect('tcp://192.168.0.212:5555')
+def send_camera():
+    context = zmq.Context()
+    footage_socket = context.socket(zmq.PUB)
+    footage_socket.connect('tcp://192.168.0.212:5555')
+    camera = cv2.VideoCapture(0)  # init the camera
+    while True:
+        try:
+            grabbed, frame = camera.read()  # grab the current frame
+            #print(grabbed, frame)
+            frame = cv2.resize(frame, (640, 480))  # resize the frame
+            encoded, buffer = cv2.imencode('.jpg', frame)
+            #cv2.imshow('abc', )
+            jpg_as_text = base64.b64encode(buffer)
 
-camera = cv2.VideoCapture(0)  # init the camera
+            footage_socket.send(jpg_as_text)
 
-
-while True:
-    try:
-        grabbed, frame = camera.read()  # grab the current frame
-        #print(grabbed, frame)
-        frame = cv2.resize(frame, (640, 480))  # resize the frame
-        encoded, buffer = cv2.imencode('.jpg', frame)
-        #cv2.imshow('abc', )
-        jpg_as_text = base64.b64encode(buffer)
-
-        footage_socket.send(jpg_as_text)
-
-    except KeyboardInterrupt:
-        camera.release()
-        cv2.destroyAllWindows()
-        break
+        except KeyboardInterrupt:
+            camera.release()
+            cv2.destroyAllWindows()
+            break
+        
+camThread = threading.Thread(target=send_camera)
+camThread.start()
