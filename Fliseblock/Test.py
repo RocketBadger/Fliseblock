503979b70c775ad670154ea326c038f77957b4d5
Tais merrild <merrildt@gmail.com>
2021-11-18 01:26:45 +0800

    opencv openmv bridge work

--- Fliseblock/Test.py
+++ Fliseblock/Test.py
@@ -1,8 +1,9 @@
 from Fliseblock import Fliseblock
 import time
-from send_cam import stream_camera
+# from send_cam import stream_camera
 from Tests import *
 from Edge_detection import stream_camera_edged
+from send_cam_openmv import *
 
 # robbie = Fliseblock()
 
@@ -13,3 +14,6 @@ from Edge_detection import stream_camera_edged
 # print("camera stream started")
 # time.sleep(10)
 # camServosTest(robbie)
+sensorOpen()
+while True:
+    sensorGetImage()
--- Fliseblock/send_cam_openmv.py
+++ Fliseblock/send_cam_openmv.py
@@ -21,8 +21,8 @@ def sensorGetImage():
     image = 0
     print("waiting...")
     try:
-        ser.flushInput()
-        print("flushed input")
+        # ser.flushInput()
+        # print("flushed input")
         image = ser.readline()
         print("got image")
         print(image)
@@ -35,10 +35,29 @@ def send_camera():
     context = zmq.Context()
     footage_socket = context.socket(zmq.PUB)
     footage_socket.connect('tcp://192.168.0.212:5555')
+    camera = cv2.VideoCapture(0)  # init the camera
+    while True:
+        try:
+            grabbed, frame = camera.read()  # grab the current frame
+            # print(grabbed, frame)
+            frame = cv2.resize(frame, (640, 480))  # resize the frame
+            encoded, buffer = cv2.imencode('.jpg', frame)
+            #cv2.imshow('abc', )
+            jpg_as_text = base64.b64encode(buffer)
+            footage_socket.send(jpg_as_text)
+        except KeyboardInterrupt:
+            camera.release()
+            cv2.destroyAllWindows()
+            break
 
-while True:
-    sensorOpen()
-    image = sensorGetImage()
+camThread = threading.Thread(target=send_camera, daemon=True)
+camThread.start()
+camThread.join()
+
+# while True:
+#     sensorOpen()
+#     sensorGetImage()
+    # print(image)
     # sensorClose()
 
 # def sensorGetTemp():
